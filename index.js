a36bb048cd5b30a95f5bd9d0a6e3c8d2186396fe
Wesley Kronmiller <31973884+The-Bob@users.noreply.github.com>
2018-03-22 15:23:25 -0700

    Whitespace

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ bot.on("ready", ()=>{
 	console.log("NO MORE SPAM!");
 });
 
-bot.on("message", function(user, userID, channelID, message, event) {
+bot.on("message", (user, userID, channelID, message, event) => {
 	messageID = event.d.id;
 	console.log("Message ID: " + messageID);
+
 	var opts = {channelID: channelID, messageID: event.d.id};
+
 	if(message.includes("hh") || message.includes("HH") || message.includes('h\nh') || message.includes("H\nH")){
 		bot.deleteMessage(opts);
 	}else if (message === lastMessage) {
+
 		bot.deleteMessages({channelID: channelID, messageIDs: [messageID, lastMessageID]}, ()=>{
 			console.log("deleted messages " + lastMessageID + " and " + messageID);
 			lastMessage = 0;
 			lastMessageID = 0;
 		});
+
 	} else{
+
 		lastMessage = message;
 		lastMessageID = messageID;
 	}
